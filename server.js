7e2ffa25ead1ebe4ac278e81fb658c3333c5cfad
israel@versatec.com.br <israel@versatec.com.br>
2017-12-29 12:11:16 -0200

    e

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // server.js
 const express = require('express');
 const app = express();
+app.engine('html', require('ejs').renderFile);
 // Run the app by serving the static files
 // in the dist directory
+// app.get('*', express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
+app.get('*', function(req, res){
+  res.render(__dirname + '/dist/index.html');
+});
 app.listen(process.env.PORT || 8080);
 console.log(`Server rodando em ${process.env.PORT || 8080}`);
